65bc63d2bf2868896c9e30651549d3d654a16e33
Joshua Luppes <cooljoebob64@gmail.com>
2020-12-23 14:19:15 -0600

    Made start game button go to game screen

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import Button from 'react-bootstrap/Button'
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
 
 class HomePage extends Component {
   render() {
     return (
-    <Jumbotron fluid>
-    <div>
-      <h1>Welcome to GOOGLE-FU!</h1>
-      <p>
-        TEST YOUR TRIVA KNOWLEDGE AGAINST YOUR FRIENDS!
-      </p>
-      <p><Button href="#">START GAME</Button> </p>
-    </div>
-  </Jumbotron>
+      <Jumbotron fluid>
+        <div>
+          <h1>Welcome to GOOGLE-FU!</h1>
+          <p>TEST YOUR TRIVA KNOWLEDGE AGAINST YOUR FRIENDS!</p>
+          <p>
+            <Button href="/game">START GAME</Button>{" "}
+          </p>
+        </div>
+      </Jumbotron>
     );
-
   }
 }
 export default HomePage;
