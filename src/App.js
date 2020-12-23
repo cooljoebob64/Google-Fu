a2fed8d86ef4c2516033ab179298f23e535b06c7
Joshua Luppes <cooljoebob64@gmail.com>
2020-12-23 13:52:16 -0600

    fixed randomfact for now

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AnotherRoundButton from "./components/AnotherRoundButton";
 import Routes from "./Routes";
-import RandomFact from "./RandomFact";
+import RandomFact from "./components/RandomFact/RandomFact";
 
 function App() {
   const debugMode = true;
--- src/components/RandomFact/randomFact.js
+++ src/components/RandomFact/randomFact.js
@@ -1,15 +1,35 @@
 import React, { Component } from "react";
 
 class RandomFact extends Component {
+  state = {
+    planetData: "",
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state.planetData = rand();
+  }
+
   render() {
     return (
       <div>
+        Here is a random fact:
         {async function randomFact() {
           let response = await fetch(
             "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?&table=exoplanets&where=*api_key=dCwVP4suoggG17puf85Cw7iZK4rWNKzrcbRFrAc0"
           );
           let user = await response.json();
-          return user;
+          user.then(
+            (res) => {
+              return res;
+            },
+            (rej) => {
+              console.log(`error: ${rej}`);
+            }
+          );
+
+          return randomFact();
         }}
       </div>
     );
