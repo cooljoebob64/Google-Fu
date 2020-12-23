2e2b30b1eea877bc888497a75bf58aee19c91cbb
Jordan Mitchell <jiminesta@gmail.com>
2020-12-23 09:33:08 -0800

    homePage panel
    updates to the Homepage and lobby page

--- src/components/LobbyPage/LobbyPage.js
+++ src/components/LobbyPage/LobbyPage.js
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
+import Table from 'react-bootstrap/Table'
 
 class LobbyPage extends Component {
   render() {
-    return <div>We are in the lobby!!!</div>;
+    return <div><Table striped bordered hover variant="dark">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Result</th>
+        <th>Win/Loss</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Player 1</td>
+        <td>Win</td>
+        <td>1/2</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Player 2</td>
+        <td>Lose</td>
+        <td>2/2</td>
+      </tr>
+    </tbody>
+  </Table></div>;
   }
 }
 export default LobbyPage;
