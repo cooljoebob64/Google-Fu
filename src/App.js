1c9d395020ac7b8e043d58c7fe8a034fca3d1f52
keyadaniel <74474124+keyadaniel@users.noreply.github.com>
2020-12-23 10:08:51 -0600

    adding AnotherRoundButton
    testing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import GameButton from "./components/GameButton/GameButton";
 import "bootstrap/dist/css/bootstrap.min.css";
+import AnotherRoundButton from "./components/AnotherRoundButton";
 
 function App() {
   return (
     <div className="App">
       <h1>Google-Fu</h1>
       <GameButton />
+      <AnotherRoundButton />
     </div>
   );
 }
